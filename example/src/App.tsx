5a49630c35ef9aa5f1e960643646f9aa9d078de8
Tasuku Murakami <>
2021-03-02 11:15:11 +0900

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-user-provider'
+import 'react-user-provider/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
